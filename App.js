bfa7f0c70a74986d7d2f888c2f8e3eb1ce21da46
kimrane13 <81587326+kimrane13@users.noreply.github.com>
2021-04-30 01:42:49 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import Exo1 from'./composant/Exo1'
+import './exo2.css'
+import './App.css'
+import './exo3.css'
+
+import Exo2 from './composant/Exo2';
+import Exo3 from './composant/Exo3'
+function App() {
+  return (
+    <div className="App">
+      <Exo1></Exo1> 
+      <Exo2></Exo2> 
+      <Exo3></Exo3>
+    </div>
+  );
+}
+
+export default App;
